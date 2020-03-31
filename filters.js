950d2bfb9c82be964ad0632926b7d8ecfe463663
FS_link <mowglysofiann@gmail.com>
2020-04-01 01:52:32 +0200

    #6 - jQuery Filters part1 :first :last

---
+++ filters.js
@@ -0,0 +1,8 @@
+// first and last
+$('#chiffre ul:first').css({border:'2px solid red'});
+$('#chiffre ul:last').css({border:'2px solid blue'});
+//first child and last child
+
+//not selector
+// const divs = $("div:not(.container)");
+// console.log(divs);
--- statement.js
+++
@@ -1,9 +0,0 @@
-let head = $('#page-title'); // return an Jquery Object which is an Array objet
-console.log(head);
-head.css({position: 'relative'});
-head.animate({left: 100});
-
-// if i catch the dom element
-head = head[0];
-// impossible to use Jquery Methods
-head.animate({right: 100});
