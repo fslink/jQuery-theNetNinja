0ef0007f4726a386a40961868af811624a18d3a0
FS_link <mowglysofiann@gmail.com>
2020-04-06 02:07:45 +0200

    #7 - Traversing the DOM with jQuery part1

---
+++ traversingDom.js
@@ -0,0 +1,10 @@
+// next and previous element
+// $('div#chiffre').next().css({border: '2px solid red'});
+// $('div#chiffre').prev().prev().css({border: '2px solid red'});
+
+//parent & parents
+// $('#chiffre').parent().css({border: '3px solid pink'});
+// $('#chiffre').parents().css({border: '3px solid pink'});
+
+//children
+$('#chiffre').children().css({border: '3px solid blue'});
