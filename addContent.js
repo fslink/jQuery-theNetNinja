41d0526799ea9d99db8f29184b74c7ab8f14063f
FS_link <mowglysofiann@gmail.com>
2020-05-05 03:10:30 +0200

    #10 - Wrap and Unwrap Elements part1

--- addContent.js
+++ addContent.js
@@ -1,12 +1,16 @@
-const tweet = '<div style="margin: 20px;'  
-+ 'padding: 10px; background: #eee">'
-+ 'The big fight live: Ham Vs Cheese !'
-+ '</div>';
+// const tweet = '<div style="margin: 20px;'  
+// + 'padding: 10px; background: #eee">'
+// + 'The big fight live: Ham Vs Cheese !'
+// + '</div>';
 
-const tweetText = 'The big fight live: Ham Vs Cheese !';
+// const tweetText = 'The big fight live: Ham Vs Cheese !';
 
 //$('ul#ul_chiffre').append(tweet);
 // $('ul#ul_chiffre').prepend(tweet);
 // $('ul#ul_chiffre').before(tweet);
 // $('ul#ul_chiffre').parent().html(tweet);
-$('ul#ul_chiffre').children().text(tweetText);
+// $('ul#ul_chiffre').children().text(tweetText);
+
+$('ul.chiffre').wrap('<div>');
+$('ul.chiffre').unwrap();
+$('ul.chiffre').wrapAll('<div id="motherfucker">');
